2ef3243c95fba2a82ceee66ad1f4ceff3e4a223f
EnriqueNav01 <64099656+EnriqueNav01@users.noreply.github.com>
2021-09-25 18:22:02 -0700

    Add files via upload

---
+++ taqueros.py
@@ -0,0 +1,16 @@
+class Taquero:
+    def __init__(self):
+        self.main_queue = []
+        self.repartidor = []
+
+    def get_order_from():
+        pass
+    
+    def send_suborder_to_wait():
+        pass
+
+    def check_suborder_finished():
+        pass
+
+    def send_order_to_chalan():
+        pass
