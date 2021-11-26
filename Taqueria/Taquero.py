1860bafee5d512ca3cc5211c10341f67cae3bc24
Ernesto Sanchez <latusa98@gmail.com>
2021-11-26 12:43:00 -0800

    -- Added ingredient checks and intial order handler

--- Taqueria/Taquero.py
+++ Taqueria/Taquero.py
@@ -1,8 +1,14 @@
 from time import sleep
+from multiprocessing import Process
 
 
 class Taquero:
     def __init__(self):
+
+        # Log steps
+        # TODO: implement logging
+        self.verbose = False
+
         # Number of maximum orders in the main queue
         self.max_queued_orders = 10
 
@@ -30,6 +36,11 @@ class Taquero:
 
         # Keep track of rests
         self.isActive = False
+        # Mock numbers, the time is defined in the project
+        self.rest_time_max = 100
+        self.rest_time = 0
+        self.active_time = 0
+        self.active_time_max = 100
 
         # Number of tacos completed
         self.tacos_done = 0
@@ -47,6 +58,10 @@ class Taquero:
 
     # This function takes care of parts
     # part is a dictionary
+    # quesadillero is an object
+    # chalan is an object
+    # returns True if the order is completed
+    # returns False if the order cannot be completed
     def process_part(self, part, quesadillero):
         # Time for each ingredient
         # This times should be the ones defined in the project
@@ -71,16 +86,36 @@ class Taquero:
 
             # Check for ingredients in part
             if 'cebolla' in part['ingredients']:
-                sleep(time_cebolla)
+                # Check if there is available ingredients
+                # if not the order is put on hold
+                if self.ingredients['cebolla'] > 0:
+                    sleep(time_cebolla)
+                else:
+                    return False
 
             if 'cilantro' in part['ingredients']:
-                sleep(time_cilantro)
+                # Check if there is available ingredients
+                # if not the order is put on hold
+                if self.ingredients['cebolla'] > 0:
+                    sleep(time_cebolla)
+                else:
+                    return False
 
             if 'guacamole' in part['ingredients']:
-                sleep(time_guacamole)
+                # Check if there is available ingredients
+                # if not the order is put on hold
+                if self.ingredients['cebolla'] > 0:
+                    sleep(time_cebolla)
+                else:
+                    return False
 
             if 'salsa' in part['ingredients']:
-                sleep(time_salsa)
+                # Check if there is available ingredients
+                # if not the order is put on hold
+                if self.ingredients['cebolla'] > 0:
+                    sleep(time_cebolla)
+                else:
+                    return False
 
             self.tacos_done += 1
 
@@ -91,3 +126,29 @@ class Taquero:
         self.done_orders.append(part)
 
         return True
+
+    # This function handles incoming orders and calls the auxiliary necessary functions
+    def handle_orders(self, order_queue, quesadillero, chalan):
+        # Work if the order queue has orders and the Taquero is not resting
+        while order_queue and self.active_time < self.active_time_max:
+            # Get an order from the order queue
+            self.main_queue.append(order_queue.get())
+
+            # Pass order to the sub queue if its not full
+            # The sub queue holds process for each part
+            if len(self.sub_queue) < self.max_sub_queued_orders:
+                # Create a process with a part from the main queue
+                p = Process(target=self.process_part, args=(self.main_queue.pop()))
+                p.start()
+                p.join()
+                self.sub_queue.append(p)
+
+            # Work on the sub queue
+            # The parts are going to be worked on using round robin
+            # The time each part of the order is worked on is defined by self.quantum
+            # TODO: implement round robin for self.sub_queue
+
+            # The chalan needs to check if an order was not completed
+            # If the order is not completed, check the ingredient pool
+            # The chalan should be another process, this so the Taquero can work in other parts
+            # TODO: implement chalan ingredient checks
