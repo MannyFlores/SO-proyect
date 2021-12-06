ab4658990ae297a8a616b69c162f0f19a6f7b581
unknown <latusa98@gmail.com>
2021-12-05 22:05:44 -0800

    added quesadillero

--- Taqueria/Quesadillero.py
+++ Taqueria/Quesadillero.py
@@ -1,23 +1,15 @@
 import time
 
 
-class Quesadillero():
+class Quesadillero:
     def __init__(self, name="Taquero"):
-
         self.name = name
-
-        # Number of tacos completed
-        self.tacos_done = 1
 
     def Do_Tacos(self, order_queue, quesadillas_queue):
         while True:
 
             part = order_queue.get()
-            quesadillas_done = 0
 
-            while quesadillas_done < part["quantity"]:
-
-                # Do quesadillas
-                # time.sleep(20)
-                quesadillas_done += 1
+            for i in range(0, part["quanitity"]):
+                time.sleep(20)
                 quesadillas_queue.put(1)
